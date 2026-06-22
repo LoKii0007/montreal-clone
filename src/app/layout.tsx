8ebdd73d18d1bfb356ff1eca125a0f3dc00ccbab
loki <lokeshyadav1098@gmail.com>
2026-06-23 00:36:55 +0530

    '

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
       </body>
     </html>
